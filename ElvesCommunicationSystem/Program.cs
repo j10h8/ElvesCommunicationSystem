b16c97c6edb385d853d11b3e7502aab57e63044f
j10h8 <john.hallbeck@outlook.com>
2024-01-15 22:20:18 +0100

    Add initial for loop layout

--- ElvesCommunicationSystem/Program.cs
+++ ElvesCommunicationSystem/Program.cs
@@ -1,10 +1,31 @@
-﻿
-string[] testInput = File.ReadAllLines("..\\..\\..\\Input\\TestInput.txt");
+﻿string[] input = File.ReadAllLines("..\\..\\..\\Input\\TestInput.txt");
 
-foreach (string line in testInput)
+for (int cycle = 0; cycle < input.Length; cycle++)
 {
-    Console.WriteLine(line);
+    if (input[cycle].StartsWith("addx"))
+    {
+        string[] splitInstruction = input[cycle].Split(' ');
+
+        if (int.TryParse(splitInstruction[1], out int V))
+        {
+            // Do stuff with V 
+        }
+        else
+        {
+            throw new Exception("Bad instruction. Could not parse value.");
+        }
+
+
+        // split and use int to alter register value
+    }
+    else
+    {
+        // noop... 
+    }
+
+    Console.WriteLine(input[cycle]);
 }
 
+Console.Write("\n");
 Console.WriteLine("Press any key to exit.");
 Console.ReadKey();
