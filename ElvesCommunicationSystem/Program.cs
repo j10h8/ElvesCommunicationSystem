6b6c39664f3fc369a78fa6b9126017ac473e6437
j10h8 <john.hallbeck@outlook.com>
2024-01-16 11:43:11 +0100

    Refactor to switch statement

--- ElvesCommunicationSystem/Program.cs
+++ ElvesCommunicationSystem/Program.cs
@@ -7,73 +7,39 @@ dataService.PrintResult("input.txt");
 char[] screenPixels = new char[240];
 int[] register = dataService.CreateRegister(dataService.ReadDataInput("input.txt"));
 
+
+int j = 0;
+
 for (int i = 0; i < screenPixels.Length; i++)
 {
-    if (i < 40)
+    switch (i)
     {
-        if (register[i] == i || register[i] == i + 1 || register[i] == i - 1)
-        {
-            screenPixels[i] = '#';
-        }
-        else
-        {
-            screenPixels[i] = '.';
-        }
+        case 40:
+            j = 40;
+            break;
+        case 80:
+            j = 80;
+            break;
+        case 120:
+            j = 120;
+            break;
+        case 160:
+            j = 160;
+            break;
+        case 200:
+            j = 200;
+            break;
+        default:
+            break;
     }
-    else if (i > 39 && i < 80)
+
+    if (register[i] == i - j || register[i] == i + 1 - j || register[i] == i - 1 - j)
     {
-        if (register[i] == i - 40 || register[i] == i + 1 - 40 || register[i] == i - 1 - 40)
-        {
-            screenPixels[i] = '#';
-        }
-        else
-        {
-            screenPixels[i] = '.';
-        }
-    }
-    else if (i > 79 && i < 120)
-    {
-        if (register[i] == i - 80 || register[i] == i + 1 - 80 || register[i] == i - 1 - 80)
-        {
-            screenPixels[i] = '#';
-        }
-        else
-        {
-            screenPixels[i] = '.';
-        }
-    }
-    else if (i > 119 && i < 159)
-    {
-        if (register[i] == i - 120 || register[i] == i + 1 - 120 || register[i] == i - 1 - 120)
-        {
-            screenPixels[i] = '#';
-        }
-        else
-        {
-            screenPixels[i] = '.';
-        }
-    }
-    else if (i > 160 && i < 200)
-    {
-        if (register[i] == i - 160 || register[i] == i + 1 - 160 || register[i] == i - 1 - 160)
-        {
-            screenPixels[i] = '#';
-        }
-        else
-        {
-            screenPixels[i] = '.';
-        }
+        screenPixels[i] = '#';
     }
     else
     {
-        if (register[i] == i - 200 || register[i] == i + 1 - 200 || register[i] == i - 1 - 200)
-        {
-            screenPixels[i] = '#';
-        }
-        else
-        {
-            screenPixels[i] = '.';
-        }
+        screenPixels[i] = '.';
     }
 }
 
