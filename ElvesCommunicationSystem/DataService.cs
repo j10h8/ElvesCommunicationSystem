bac99018319451e11b39eef1fbe8eda98c69159f
j10h8 <john.hallbeck@outlook.com>
2024-01-16 09:24:11 +0100

    Refactor

---
+++ ElvesCommunicationSystem/DataService.cs
@@ -0,0 +1,114 @@
+﻿namespace ElvesCommunicationSystem
+{
+    public class DataService
+    {
+        public string[] ReadDataInput(string fileName)
+        {
+            return File.ReadAllLines($"..\\..\\..\\Input\\{fileName}");
+        }
+
+        public int[] CreateRegister(string[] input)
+        {
+            int numberOfCycles = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].StartsWith("addx"))
+                {
+                    numberOfCycles += 2;
+                }
+                else
+                {
+                    numberOfCycles++;
+                }
+            }
+
+            int[] register = new int[numberOfCycles];
+
+            GenerateRegisterValues(input, register);
+
+            return register;
+        }
+
+        public void GenerateRegisterValues(string[] input, int[] register)
+        {
+            int cycle = 0;
+            int registerValue = 1;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].StartsWith("addx"))
+                {
+                    register[cycle] = registerValue;
+                    cycle++;
+                    register[cycle] = registerValue;
+                    cycle++;
+
+                    string[] splitInstruction = input[i].Split(' ');
+
+                    if (int.TryParse(splitInstruction[1], out int V))
+                    {
+                        registerValue += V;
+                    }
+                    else
+                    {
+                        throw new Exception("Bad instruction. Could not parse V value.");
+                    }
+                }
+                else
+                {
+                    register[cycle] = registerValue;
+                    cycle++;
+                }
+            }
+        }
+
+        public int GetSumOfSignalStrengths(int[] register)
+        {
+            if (register.Length < 19)
+            {
+                throw new Exception("Insufficient number of instructions. Could not get sum of signal strengths.");
+            }
+            else
+            {
+                int sumOfSignalStrengths = 0;
+                int i = 19;
+
+                while (i < 220)
+                {
+                    sumOfSignalStrengths += (register[i] * (i + 1));
+
+                    i = i + 40;
+                }
+
+                return sumOfSignalStrengths;
+            }
+        }
+
+        public void PrintResult(string fileName)
+        {
+            int[] register = CreateRegister(ReadDataInput(fileName));
+            int sumOfSignalStrengths = GetSumOfSignalStrengths(register);
+
+            for (int c = 0; c < register.Length; c++)
+            {
+                Console.WriteLine($"{c + 1} {register[c]}");
+            }
+
+            Console.Write("\n");
+
+            int d = 20;
+            while (d <= 220)
+            {
+                Console.WriteLine("  " + register[d - 1] * d + $" ({register[d - 1]} * {d})");
+                d = d + 40;
+            }
+
+            Console.WriteLine("+ _______________");
+            Console.WriteLine("  " + sumOfSignalStrengths);
+            Console.Write("\n");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
--- ElvesCommunicationSystem/Program.cs
+++ ElvesCommunicationSystem/Program.cs
@@ -1,86 +1,92 @@
-﻿// Read values from input file and store in array 
-string[] input = File.ReadAllLines("..\\..\\..\\Input\\input.txt");
+﻿using ElvesCommunicationSystem;
 
-#region Determine number of cycles that input generates, and create array based on result 
-int numberOfCycles = 0;
+DataService dataService = new DataService();
 
-for (int h = 0; h < input.Length; h++)
-{
-    if (input[h].StartsWith("addx"))
-    {
-        numberOfCycles += 2;
-    }
-    else
-    {
-        numberOfCycles++;
-    }
-}
+dataService.PrintResult("input.txt");
 
-int[] registerX = new int[numberOfCycles];
-#endregion
+//// Read values from input file and store in array 
+//string[] input = File.ReadAllLines("..\\..\\..\\Input\\input.txt");
 
-#region Iterate over input to generate X values for CPU register 
-int cycle = 0;
-int registerXValue = 1;
+//#region Determine number of cycles that input generates, and create array based on result 
+//int numberOfCycles = 0;
 
-for (int i = 0; i < input.Length; i++)
-{
-    if (input[i].StartsWith("addx"))
-    {
-        registerX[cycle] = registerXValue;
-        cycle++;
-        registerX[cycle] = registerXValue;
-        cycle++;
+//for (int h = 0; h < input.Length; h++)
+//{
+//    if (input[h].StartsWith("addx"))
+//    {
+//        numberOfCycles += 2;
+//    }
+//    else
+//    {
+//        numberOfCycles++;
+//    }
+//}
 
-        string[] splitInstruction = input[i].Split(' ');
+//int[] registerX = new int[numberOfCycles];
+//#endregion
 
-        if (int.TryParse(splitInstruction[1], out int V))
-        {
-            registerXValue += V;
-        }
-        else
-        {
-            throw new Exception("Bad instruction (could not parse V value)");
-        }
-    }
-    else
-    {
-        registerX[cycle] = registerXValue;
-        cycle++;
-    }
-}
-#endregion
+//#region Iterate over input to generate X values for CPU register 
+//int cycle = 0;
+//int registerXValue = 1;
 
-#region Calculate requested value 
-int sumOfSignalStrengths = 0;
-int j = 19;
+//for (int i = 0; i < input.Length; i++)
+//{
+//    if (input[i].StartsWith("addx"))
+//    {
+//        registerX[cycle] = registerXValue;
+//        cycle++;
+//        registerX[cycle] = registerXValue;
+//        cycle++;
 
-while (j < 220)
-{
-    sumOfSignalStrengths += (registerX[j] * (j + 1));
+//        string[] splitInstruction = input[i].Split(' ');
 
-    j = j + 40;
-}
-#endregion
+//        if (int.TryParse(splitInstruction[1], out int V))
+//        {
+//            registerXValue += V;
+//        }
+//        else
+//        {
+//            throw new Exception("Bad instruction (could not parse V value)");
+//        }
+//    }
+//    else
+//    {
+//        registerX[cycle] = registerXValue;
+//        cycle++;
+//    }
+//}
+//#endregion
 
-#region Print result to console 
-for (int c = 0; c < registerX.Length; c++)
-{
-    Console.WriteLine($"{c + 1} {registerX[c]}");
-}
+//#region Calculate requested value 
+//int sumOfSignalStrengths = 0;
+//int j = 19;
 
-Console.Write("\n");
+//while (j < 220)
+//{
+//    sumOfSignalStrengths += (registerX[j] * (j + 1));
 
-int d = 20;
-while (d <= 220)
-{
-    Console.WriteLine("  " + registerX[d - 1] * d + $" ({registerX[d - 1]} * {d})");
-    d = d + 40;
-}
+//    j = j + 40;
+//}
+//#endregion
 
-Console.WriteLine("+ _______________");
-Console.WriteLine("  " + sumOfSignalStrengths);
-Console.Write("\n");
-Console.WriteLine("Press any key to exit.");
-Console.ReadKey();
-#endregion
+//#region Print result to console 
+//for (int c = 0; c < registerX.Length; c++)
+//{
+//    Console.WriteLine($"{c + 1} {registerX[c]}");
+//}
+
+//Console.Write("\n");
+
+//int d = 20;
+//while (d <= 220)
+//{
+//    Console.WriteLine("  " + registerX[d - 1] * d + $" ({registerX[d - 1]} * {d})");
+//    d = d + 40;
+//}
+
+//Console.WriteLine("+ _______________");
+//Console.WriteLine("  " + sumOfSignalStrengths);
+//Console.Write("\n");
+//Console.WriteLine("Press any key to exit.");
+//Console.ReadKey();
+//#endregion
